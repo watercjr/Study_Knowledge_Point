693d480c86d657c641762e615ee498abe4b67351
Junren.Chen <Junren.Chen@desay-svautomotive.com>
2020-12-14 11:02:57 +0800

    [GDB_Using_Example][Test case for GDB]
    Signed-off-by: Junren.Chen <Junren.Chen@desay-svautomotive.com>

---
+++ GDB_Using_Example/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Program Begin" << endl;
+    cout << "Hello World!" << endl;
+    cout << "Program End" << endl;
+
+    return 0;
+}
